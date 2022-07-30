c41abf7d7085c947029d4791dedb1cd263a80f42
Alexei <i0ver@mail.ru>
2022-07-30 14:13:46 +0300

    refactoring + selector

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import {ChangeEvent, useState} from "react";
 import {v1} from "uuid";
 
 function LongCommentChecker() {
-    const [users, setUsers] = useState( [
+    const [name, setName] = useState("click me")
+    const [users, setUsers] = useState([
         {id: v1(), name: "Alex", isDone: false},
         {id: v1(), name: "Nick", isDone: false},
         {id: v1(), name: "John", isDone: false},
@@ -15,13 +16,12 @@ function LongCommentChecker() {
     const [check, setCheck] = useState(false)
     const [click, setClick] = useState(false)
     const [error, setError] = useState(false)
-    const [valueSpan, setValueSpan] = useState(users[0].name)
-    const [spanInfo, setSpanInfo] = useState(users[0].name)
+    const [spanValue, setSpanValue] = useState(users[0].name)
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setValue(e.currentTarget.value)
     }
-    const onChangeChecked = (e:ChangeEvent<HTMLInputElement>) => {
+    const onChangeChecked = (e: ChangeEvent<HTMLInputElement>) => {
         setCheck(e.currentTarget.checked)
     }
     const onClickHandler = () => {
@@ -38,49 +38,60 @@ function LongCommentChecker() {
         setError(true)
     }
     const changeInput = () => {
-        setSpanInfo(valueSpan)
+        setSpanValue(spanValue)
         setError(false)
     }
-
     const changeSpanValue = (e: ChangeEvent<HTMLInputElement>) => {
-        setValueSpan(e.currentTarget.value)
+        setSpanValue(e.currentTarget.value)
     }
 
     return (
-        <div  className={"css"}>
+        <div className={"css"}>
             <div>
                 <input placeholder={"Etner your task..."} type="text" onChange={onChangeHandler} value={value}/>
                 <button disabled={!value} onClick={addTask}>add Task</button>
             </div>
             <label><input type="checkbox" checked={check} onChange={onChangeChecked}/>
                 <span className={check ? 'green' : 'red'}>{check.toString()}</span></label>
-            <h3 className={click ? "h3" : ""} onClick={onClickHandler}>click me</h3>
-            { click && <ul>
+            <h3 className={click ? "h3" : ""} onClick={onClickHandler}>{name}</h3>
+            {click && <ul>
                 {users.map(el => {
-                    const onChangeChecked = (e:ChangeEvent<HTMLInputElement>) => {
-                        setUsers(users.map(n => n.id === el.id ? {...n, isDone: e.currentTarget.checked} : n))
-                    }
-                    const onClickDelete = () => {
-                        setUsers(users.filter(n => n.id !== el.id))
-                    }
+                        const onChangeChecked = (e: ChangeEvent<HTMLInputElement>) => {
+                            setUsers(users.map(n => n.id === el.id ? {...n, isDone: e.currentTarget.checked} : n))
+                        }
+                        const onClickDelete = () => {
+                            setUsers(users.filter(n => n.id !== el.id))
+                        }
+                        const renameSpan = () => {
+                            setSpanValue(el.name)
+                            setName(el.name)
+                        }
 
-                return (
-                    <li key={el.id} className={el.isDone ? 'checkOn' : 'checkOff'}><label><input onChange={onChangeChecked} type="checkbox" checked={el.isDone}/>{el.name}</label><button onClick={onClickDelete}>X</button></li>
-                )
-                }
+                        return (
+                            <li key={el.id} className={el.isDone ? 'checkOn' : 'checkOff'}>
+                                <input onChange={onChangeChecked} type="checkbox"
+                                       checked={el.isDone}/>
+                                <span onClick={renameSpan}>{el.name}</span>
+                                <button onClick={onClickDelete}>X</button>
+                            </li>
+                        )
+                    }
                 )}
-            </ul> }
+            </ul>}
             <div className={'value'}>{value}</div>
-            { error
-            ? <input onBlur={changeInput} onChange={changeSpanValue} value={valueSpan} autoFocus/>
-            : <span onDoubleClick={changeSpan}>{spanInfo}</span> }
+            {error
+                ? <input onBlur={changeInput} onChange={changeSpanValue} value={spanValue} autoFocus/>
+                : <span onDoubleClick={changeSpan}>{spanValue}</span>
+            }
+            <div>
+                <select>
+                    <option className={"opt"}>Minsk</option>
+                    <option value="">Moscow</option>
+                    <option value="">Kiev</option>
+                </select>
+            </div>
         </div>
     )
 }
 
 ReactDOM.render(<LongCommentChecker/>, document.getElementById('root'));
-
-// Что нужно написать вместо XXX, чтобы кнопка отправки комментария отрабатывала верно:
-// первоначально кнопка должна быть в состоянии disable, а после успешного выполнения условия
-// (проверка на длину комментария) должна раздизаблиться.
-// ❗ Ответ необходимо дать на основании данных (переменных), которые уже есть в коде
